b329b8349c79f74fcd3f211a5848fcd20cf7b57b
Justin Cook <jhcook@secnix.com>
2014-09-24 17:33:03 +0100

    Added "some" dictionary functionality.

--- dictionary/dictionary.go
+++ dictionary/dictionary.go
@@ -8,11 +8,16 @@ Author: Justin Cook <jhcook@gmail.com>
 package dictionary
 
 import (
-//	"fmt"
 	"log"
 	"os"
 	"bufio"
 )
+
+type dictionary struct {
+	word string
+	ci uint32
+	words []string
+}
 
 // readLines reads a whole file into memory
 // and returns a slice of its lines.
@@ -30,4 +35,19 @@ func readLines(path string) ([]string, error) {
 		lines = append(lines, scanner.Text())
 	}
 	return lines, scanner.Err()
+}
+
+func NewDictionary(path string) *dictionary {
+	dict := new(dictionary)
+	if len(path) == 0 {
+		path = "/usr/share/dict/words"
+	}
+	dict.words, _ = readLines(path)
+	return dict
+}
+
+func (d *dictionary) NextWord() string {
+	d.ci++
+	d.word = d.words[d.ci]
+	return d.word
 }
--- hangman/hangman.go
+++ hangman/hangman.go
@@ -4,7 +4,7 @@ import (
     "log"
     "net/http"
     "encoding/json"
-//	"github.com/jhcook/game_engine/dictionary"
+	"github.com/jhcook/game_engine/dictionary"
 )
 
 type Hangman struct {
@@ -14,7 +14,9 @@ type Hangman struct {
 }
 
 func Play(w http.ResponseWriter, r *http.Request) {
-    game := Hangman{"fuck", []rune("Justin"), 1234567}
+    //game := Hangman{"fuck", []rune("Justin"), 1234567}
+	dict := dictionary.NewDictionary("")
+	game := Hangman{dict.NextWord(), []rune("Justin"), 1234567}
     if bytes, err := json.Marshal(game); err == nil {
         log.Println(string(bytes))
         w.Write(bytes)
