62d2dab7fc302bb2f29d2d5698f2ab53099af392
Justin Cook <jhcook@secnix.com>
2014-09-25 22:14:22 +0100

    Add JSON functionality inbound. Added the Message struct and start adding commands.

--- hangman/hangman.go
+++ hangman/hangman.go
@@ -1,13 +1,21 @@
+/*
+Package hangman is an implementation of the game Hangman many of us played
+as a child.
+
+Author: Justin Cook <jhcook@gmail.com>
+*/
+
 package hangman
 
 import (
 	"encoding/json"
 	"github.com/jhcook/game_engine/dictionary"
-	//	"github.com/jhcook/game_engine/util"
+	"github.com/jhcook/game_engine/util"
 	"log"
 	"math/rand"
 	"net/http"
 	"time"
+	"unicode/utf8"
 )
 
 var theBoys *men = nil
@@ -15,9 +23,17 @@ var dict *dictionary.Dictionary = nil
 
 type hangman struct {
 	Word   string
-	Played []rune
+	WrdUni []rune
+	Right  []rune
+	Wrong  []rune
 	Game   uint64
 	Timer  *time.Timer
+}
+
+type Message struct {
+	Cmd  string
+	Gid  uint64
+	Auth string
 }
 
 type men struct {
@@ -40,19 +56,37 @@ func NewMen() {
 // to the caller.
 func NewHangman() *hangman {
 	dict.Ci = uint32(rand.Intn(len(dict.Words)))
+	wrd := dict.NextWord()
 	theBoys.Games++
-	game := &hangman{Word: dict.NextWord(),
-		Played: make([]rune, 0, 256),
+	game := &hangman{Word: wrd,
+		WrdUni: util.StringToRuneArray(wrd),
+		Right:  make([]rune, len(wrd)),
+		Wrong:  make([]rune, 0, 256),
 		Game:   theBoys.Games,
 		Timer:  time.NewTimer(time.Second * 300)}
 	theBoys.Episode[theBoys.Games] = game
 	return game
 }
 
-func Play(w http.ResponseWriter, r *http.Request) {
-	// Just for initial development create a new game on each play attempt.
+func (g *hangman) evalChar(chr string) bool {
+	var correct bool = false
+	// Convert chr to rune
+	letter, _ := utf8.DecodeRuneInString(chr)
+	// See if rune is in the word
+	for i, v := range g.WrdUni {
+		if letter == v {
+			g.Right[i] = letter
+			correct = true
+		}
+	}
+	if !correct {
+		g.Wrong = append(g.Wrong, letter)
+	}
+	return correct
+}
+
+func NewGame() *hangman {
 	h := NewHangman()
-	// On each play, reset the timer
 	h.Timer = time.NewTimer(time.Second * 60)
 	// If the timer expires, remove the game from theBoys and free memory
 	go func() {
@@ -62,13 +96,37 @@ func Play(w http.ResponseWriter, r *http.Request) {
 			delete(theBoys.Episode, h.Game)
 		}
 	}()
-	// In the real world we will use an anonymous structure to sanitize the
-	// data sent back.
+	return h
+}
+
+func Play(w http.ResponseWriter, r *http.Request) {
+	// Decode the JSON
+	decoder := json.NewDecoder(r.Body)
+	var msg Message
+	var game *hangman
+	if err := decoder.Decode(&msg); err != nil {
+		log.Println("hangman.Play.Decode:", err)
+		return
+	}
+	if len(msg.Cmd) > 0 && msg.Gid > 0 && len(msg.Auth) > 0 {
+		if game, ok := theBoys.Episode[msg.Gid]; ok {
+			log.Println(game)
+		} else {
+			return
+		}
+	} else if msg.Cmd == "NEW" {
+		game = NewHangman()
+	} else {
+		return
+	}
+	// On each play, reset the timer
+	game.Timer.Reset(time.Second * 300)
+	// Use an anonymous structure to sanitize the data sent back.
 	answer := struct {
 		Word   string
 		Missed []rune
 		Game   uint64
-	}{h.Word, h.Played, h.Game}
+	}{game.Word, game.Wrong, game.Game}
 	// Send the result down the wire.
 	if bytes, err := json.Marshal(answer); err == nil {
 		log.Println(string(bytes))
