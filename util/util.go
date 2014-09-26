5dea739cf8c953de3485f4aa7c96712a2152f111
Justin Cook <jhcook@secnix.com>
2014-09-26 12:10:17 +0100

    Change random string generator from randbo to crypto/rand

--- util/util.go
+++ util/util.go
@@ -4,9 +4,7 @@ import (
 	"bytes"
 	"strconv"
 	"unicode/utf8"
-	"io"
-	"math/rand"
-	"time"
+	"crypto/rand"
 )
 
 func StringToRuneArray(str string) []rune {
@@ -31,35 +29,12 @@ func RuneToString(rstr []rune) string {
 	return buffer.String()
 }
 
-// Randbo creates a stream of non-crypto quality random bytes
-type randbo struct {
-	rand.Source
-}
-
-// New creates a new random reader with a time source.
-func New() io.Reader {
-	return NewFrom(rand.NewSource(time.Now().UnixNano()))
-}
-
-// NewFrom creates a new reader from your own rand.Source
-func NewFrom(src rand.Source) io.Reader {
-	return &randbo{src}
-}
-
-// Read satisfies io.Reader
-func (r *randbo) Read(p []byte) (n int, err error) {
-	todo := len(p)
-	offset := 0
-	for {
-		val := int64(r.Int63())
-		for i := 0; i < 8; i++ {
-			p[offset] = byte(val)
-			todo--
-			if todo == 0 {
-				return len(p), nil
-			}
-			offset++
-			val >>= 8
-		}
+func Rand_str(str_size int) string {
+	alphanum := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, str_size)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		bytes[i] = alphanum[b%byte(len(alphanum))]
 	}
+	return string(bytes)
 }
